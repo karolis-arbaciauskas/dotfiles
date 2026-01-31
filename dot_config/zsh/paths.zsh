65b4ec12fa765e4ff5edce1e9917fd4199591c58
Karolis Arbaciauskas <karolis.arbaciauskas@vinted.com>
2026-01-31 15:41:11 +0200

    Add zoxide

--- dot_config/zsh/paths.zsh
+++ dot_config/zsh/paths.zsh
@@ -1,5 +1,6 @@
 if command -v brew >/dev/null 2>&1; then
   BREW_PREFIX="${BREW_PREFIX:-$(brew --prefix)}"
   export PATH="$BREW_PREFIX/opt/ruby/bin:$PATH"
+  fpath=("$BREW_PREFIX/share/zsh/site-functions" $fpath)
 fi
 export PATH="$GOPATH/bin:$PATH"
--- dot_config/zsh/tools.zsh
+++ dot_config/zsh/tools.zsh
@@ -4,6 +4,9 @@ eval "$(starship init zsh)"
 # mise
 eval "$(mise activate zsh)"
 
+# zoxide
+eval "$(zoxide init zsh)"
+
 # fzf + rg + fd
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
